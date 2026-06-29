e124e26ed88a9033485a2dac835fcd657a148576
harshiltewari2004 <harshiltewari2004@gmail.com>
2026-06-29 13:18:04 +0530

    Add SubmissionWriter PRACTICE path with all-tags solves increment

---
+++ server/ingest/SubmissionWriter.js
@@ -0,0 +1,42 @@
+import Submission from '../models/Submission.js';
+import TopicBucketScore from '../models/TopicBucketScore.js';
+import logger from '../config/logger.js';
+
+const DUPLICATE_KEY_ERROR=11000;
+
+export const writeSubmission = async(submission,context)=>{
+    switch (submission.participantType){
+        case 'PRACTICE':
+            return writePracticeSubmission(submission,context);
+        case 'CONTESTANT':
+            throw new Error('CONTESTANT path not implemented yet');
+        case 'VIRTUAL':
+            throw new Error(`Unknown participantType :${submission.participantType}`);
+    }
+};
+
+const writePracticeSubmission = async(submission,context)=>{
+    const {userId,problem} = context;
+
+    try{
+        await Submission.create(submission);
+    }
+    catch(err){
+        if(err.code===DUPLICATE_KEY_ERROR){
+            return;
+        }
+        throw err;
+    }
+
+    if(submission.verdict!=='OK'){
+        return;
+    }
+    for(const tag of problem.tags){
+        await TopicBucketScore.updateOne(
+            {user:userId,topic:tag,bucket:problem.ratingBucket},
+            {$inc:{solves:1}},
+            {upsert:true}
+        );
+    }
+
+};
